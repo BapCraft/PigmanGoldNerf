fece3d5e69e3c063f5b5fb8e38fb1450a1a10dce
treyzania <Treyzania@gmail.com>
2016-05-28 14:14:15 -0400

    Set up Maven and basic suppression logic.

---
+++ src/main/java/com/bapcraft/nerf/pigman/PigmanDropSuppressor.java
@@ -0,0 +1,38 @@
+package com.bapcraft.nerf.pigman;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Monster;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+
+public class PigmanDropSuppressor implements Listener {
+	
+	public List<Monster> blockedDrops = new ArrayList<>();
+	
+	public void onEntitySpawn(EntitySpawnEvent event) {
+		
+		Entity e = event.getEntity();
+		EntityType type = e.getType();
+		
+		if (type == EntityType.PIG_ZOMBIE && PigmanGoldNerf.INSTANCE.hasBlockedDrops(e.getWorld())) {
+			this.blockedDrops.add((Monster) e);
+		}
+		
+	}
+	
+	public void onEntityDeath(EntityDeathEvent event) {
+		
+		Entity e = event.getEntity();
+		
+		if (this.blockedDrops.contains(e)) {
+			event.getDrops().clear();
+		}
+		
+	}
+	
+}
---
+++ src/main/java/com/bapcraft/nerf/pigman/PigmanGoldNerf.java
@@ -0,0 +1,35 @@
+package com.bapcraft.nerf.pigman;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PigmanGoldNerf extends JavaPlugin {
+
+	public static PigmanGoldNerf INSTANCE;
+	
+	@Override
+	public void onEnable() {
+		
+		INSTANCE = this;
+		
+		// Register the listener.
+		this.getPluginLoader().createRegisteredListeners(new PigmanDropSuppressor(), this);
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+		INSTANCE = null;
+		
+	}
+	
+	public boolean hasBlockedDrops(World world) {
+		
+		// TODO Use configs.
+		return world.getName().equals(Bukkit.getWorlds().get(0).getName());
+		
+	}
+	
+}
